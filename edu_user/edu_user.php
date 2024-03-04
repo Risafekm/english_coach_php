ceb76fb51f3c0459c1f755e0833b0796dcb4f6f6
sana fathima <136157669+sanafathima7@users.noreply.github.com>
2024-03-04 20:38:42 +0530

    Update edu_user.php

--- edu_user/edu_user.php
+++ edu_user/edu_user.php
@@ -18,7 +18,7 @@ function updateUser($userInput, $userparams){
     global $conn;
 
     // Check if required parameters are set
-    if(!isset($userparams['user_name'])) {
+    if(!isset($userparams['user_email'])) {
         return error422('User name is not found in URL');
     } elseif(!isset($userInput['user_pswd'])){
         return error422('Password is missing');
@@ -29,11 +29,11 @@ function updateUser($userInput, $userparams){
     }
 
     // Escape user inputs to prevent SQL injection
-    $user_name = mysqli_real_escape_string($conn, $userparams['user_name']);
+    $user_name = mysqli_real_escape_string($conn, $userparams['user_email']);
     $user_pswd = mysqli_real_escape_string($conn, $userInput['user_pswd']);
 
     // Update query
-    $query = "UPDATE `edu_users` SET `user_pswd` = '$user_pswd' WHERE `user_name` = '$user_name' LIMIT 1";
+    $query = "UPDATE `edu_users` SET `user_pswd` = '$user_pswd' WHERE `user_email` = '$user_name' LIMIT 1";
 
     // Perform the update query
     $result = mysqli_query($conn, $query);
@@ -64,16 +64,16 @@ function updateUser($userInput, $userparams){
     function getUser($userInput) {
         global $conn;
     
-        if (!isset($userInput['user_name']) || !isset($userInput['user_pswd'])) {
+        if (!isset($userInput['user_email']) || !isset($userInput['user_pswd'])) {
             return error422('Username and password are required');
         }
     
-        $username = mysqli_real_escape_string($conn, $userInput['user_name']);
+        $username = mysqli_real_escape_string($conn, $userInput['user_email']);
         $password = mysqli_real_escape_string($conn, $userInput['user_pswd']);
         $hashedPassword = md5($password);
     
         // Prepare and execute SQL query
-        $query = "SELECT * FROM edu_users WHERE user_name = '$username' AND user_pswd = '$password'";
+        $query = "SELECT * FROM edu_users WHERE user_email = '$username' AND user_pswd = '$password'";
         $result = mysqli_query($conn, $query);
     
         // Check if the query was successful
