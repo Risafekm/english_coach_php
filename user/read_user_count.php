e19281aaf088c659aa284242224b94e6cfb90892
RISAF KM <101542049+Risafekm@users.noreply.github.com>
2024-03-25 14:36:55 +0530

    user,module,trail_modules count done

---
+++ user/read_user_count.php
@@ -0,0 +1,30 @@
+
+<?php
+// error_reporting(0);
+header("Access-Control-Allow-Origin:*");
+header("Content-Type: application/json");
+header("Access-Control-Allow-Methods: GET");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+include('user_count_function.php');
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if($requestMethod == 'GET'){
+
+$getUserCount = getUserCount();
+echo $getUserCount;
+
+}else{
+$data = [
+    'status' => 405,
+    'message' => $requestMethod. 'Method Not Allowed',
+];
+header("HTTP/1.0 405 Method Not Allowed");
+echo json_encode($data);
+}
+
+?>
+
+//http://localhost/english_coach_php/user/read_user_count.php
