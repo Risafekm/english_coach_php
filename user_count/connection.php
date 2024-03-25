15153f07a7fdd590e469acca41884c6db1d98381
Rusafida <139863997+Rusafida@users.noreply.github.com>
2024-03-25 12:49:14 +0530

    Add files via upload
    user count done

---
+++ user_count/connection.php
@@ -0,0 +1,10 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$dbname = "new_db";
+$conn = new mysqli($hostname, $username, $password, $dbname);
+if($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
